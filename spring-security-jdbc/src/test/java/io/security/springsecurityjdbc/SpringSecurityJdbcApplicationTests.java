dec1156252a6fdc242f5e7470d1cab19d8f8d880
Adrian Wilk <awilk2@gmail.com>
2020-06-30 15:33:51 +0200

    create simple security with project with jdbc

---
+++ spring-security-jdbc/src/test/java/io/security/springsecurityjdbc/SpringSecurityJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.springsecurityjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
